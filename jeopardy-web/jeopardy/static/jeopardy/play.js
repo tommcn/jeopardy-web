cfccb2cc058d27720ebc31f0ffca1347c16511d4
tommcn <tommcn@mcnamer.ca>
2020-09-19 13:35:05 -0400

    better game

--- jeopardy-web/jeopardy/static/jeopardy/play.js
+++ jeopardy-web/jeopardy/static/jeopardy/play.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    
     const categories = JSON.parse(document.getElementById('cats').textContent);
     const table = $('#jeopardy-table');
     const questions_per_cat = categories[0].questions.length;
@@ -19,17 +20,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
     {
         const questions = [];
         categories.forEach((el, idx) => {
-            console.log(el)
-            const button = $(`<span id="q-${i}-${idx}" onclick="alert('Question: ${el.questions[i].question} for ${el.questions[i].points.toString()}'); $('#q-${i}-${idx}').addClass('asked'); $('#q-${i}-${idx}').removeAttr('onclick');"></span>`).text(el.questions[i].points);
+            const button = $(`<span id="q-${i}-${idx}" onclick="showQuestion('${el.questions[i].question}', ${el.questions[i].points}); $('#q-${i}-${idx}').addClass('asked'); $('#q-${i}-${idx}').removeAttr('onclick');"></span>`).text(el.questions[i].points);
             questions.push($('<td></td>').append(button));
         });
         tbody.append($(`<tr id=row-${i}></tr>`).append(questions))
     }
 
     table.append(tbody);
-    // Add questions to body
-
-
-
-    console.log(categories);
+    
 });
+
+
+
+
+function showQuestion(question, points) 
+{
+    $('#exampleModal').modal('show');
+    $('.modal-body').text(question);
+    $('.modal-backdrop').detach().appendTo('#fullscreen-container');
+    
+    // alert(question + " \n Points: " + points.toString());
+}
