1be7a0d3d8b6363f0307858031b32e88754f615e
tommcn <tommcn@mcnamer.ca>
2020-09-24 20:23:01 -0400

    Stuuf I forgot to commit lol

--- jeopardy-web/jeopardy/static/jeopardy/play.js
+++ jeopardy-web/jeopardy/static/jeopardy/play.js
@@ -8,14 +8,20 @@ function createData(categories)
 {
     const table = $('#jeopardy-table');
     table.html('');
-    const questions_per_cat = categories[0].questions.length;
+    const questions_per_cat = categories[0].questions.length; // minus final cate
     // const number_of_cats = categories.length;
 
     // Create Headers
     const thead = $('<thead></thead>');
     const row = $('<tr></tr>');
     categories.forEach((el, i) => {
-        row.append($("<th></th>").text(el.name));
+        if (!el.isFinal)
+        {
+
+            row.append($("<th></th>").text(el.name));
+        } else {
+            $('.FJ-modal-body').text(el.question);
+        }
     });
     thead.append(row);
     table.append(thead);
@@ -25,10 +31,16 @@ function createData(categories)
     for (var i = 0; i < questions_per_cat; i++ )
     {
         const questions = [];
+        
         categories.forEach((el, idx) => {
-            console.log(el.questions[i].question);
-            const button = $(`<span id="q-${i}-${idx}" onclick="showQuestion(\`${el.questions[i].question.replaceAll(new RegExp('"', 'g'), "&quot;")}\`, ${el.questions[i].points}); $('#q-${i}-${idx}').addClass('asked');"></span>`).text(el.questions[i].points); //  remove aattr onclick
-            questions.push($('<td></td>').append(button));
+            if (el.isFinal)
+            {
+                console.log("asdasd")
+            }  else {
+                console.log(el.questions[i].question);
+                const button = $(`<span class="q" id="q-${i}-${idx}" onclick="showQuestion(\`${el.questions[i].question.replaceAll(new RegExp('"', 'g'), "&quot;")}\`, ${el.questions[i].points}); $('#q-${i}-${idx}').addClass('asked');"></span>`).text(el.questions[i].points); //  remove aattr onclick
+                questions.push($('<td></td>').append(button));
+            }
         });
         tbody.append($(`<tr id=row-${i}></tr>`).append(questions))
     }
@@ -43,6 +55,14 @@ function showQuestion(question, points)
     $('#exampleModal').modal('show');
     $('.modal-body').text(question);
     $('.modal-backdrop').detach().appendTo('#fullscreen-container');
+    $('#exampleModal').on('hidden.bs.modal', function (e) {
+        if ($('.asked').length === $('.q').length)
+        {
+            console.log("DONE!")
+            $('.FJ-modal-body').addClass("enabled")
+        
+        }
+    })
     
     // alert(question + " \n Points: " + points.toString());
 }
